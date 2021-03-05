7364d5dafcbc4e035e18fc22d07a5f32ccc88e32
ali <info@ginyatov.com>
2021-03-05 23:17:30 +0300

    feat(db): add db
    * add typeorm
    
    Closes #6

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
+import { environment } from '../environments/environment';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmCoreModule.forRoot({
+      ...environment.connection
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
